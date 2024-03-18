ff1305b12c69a5efbebd42ebc423230131cdc41c
mikewanjero <mikewanj@gmail.com>
2024-03-18 15:52:25 +0300

    Added MaterialIcons and edited them

--- components/toDoForm.js
+++ components/toDoForm.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export function ToDoForm({ item, pressHandler }) {
   return (
     <TouchableOpacity onPress={() => pressHandler(item.key)}>
-      <Text style={styles.item}>{item.text}</Text>
+      <View style={styles.item}>
+        <MaterialIcons name="delete" size={18} color={"#333"} />
+        <Text style={styles.itemText}>{item.text}</Text>
+      </View>
     </TouchableOpacity>
   );
 }
@@ -17,5 +21,9 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderStyle: "dashed",
     borderRadius: 10,
+    flexDirection: "row",
+  },
+  itemText: {
+    marginLeft: 10,
   },
 });
