5688e67090d9bf867fb7536371a0cb83ebf87667
mikewanjero <mikewanj@gmail.com>
2024-03-19 09:19:57 +0300

    Changed button colour

--- components/addToDo.js
+++ components/addToDo.js
@@ -30,6 +30,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 8,
     paddingVertical: 6,
     borderBottomWidth: 1,
-    borderBottomColor: "#ddd",
+    borderBottomColor: "#fff",
   },
 });
